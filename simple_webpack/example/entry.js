fd869f5b3ddd201f18e57546a43a6b51d4638780
Lucy20209060 <550502661@qq.com>
2021-06-20 23:51:15 +0800

    简易webpack

---
+++ simple_webpack/example/entry.js
@@ -0,0 +1,6 @@
+import message from './message.js'
+import { name2, name3 } from './name.js'
+
+console.log(message)
+console.log(name2)
+console.log(name3)
---
+++ simple_webpack/example/name.js
@@ -0,0 +1,3 @@
+export const name = 'world'
+export const name2 = 'world2'
+export const name3 = 'world3'
