2c7ab69866f54bc8f1119c911e0e56346b96a37f
长剑腥腥挂壁 <550502661@qq.com>
2021-07-09 18:10:10 +0800

    Update index.js

--- react_simple/index.js
+++ react_simple/index.js
@@ -1,10 +1,18 @@
-import React from './react'
+import React from './react';
+import ReactDOM from './react-dom';
+
+function FunctionComponent(props) {
+    return <p>函数组件-{props.name}</p>;
+}
 
 const element = (
-  <div className="title">
-    hello <span>react</span>
-  </div>
-)
-console.log(element)
+    <div className="title" style={{ color: 'red', fontSize: 20 }} onClick={() => console.log(1)}>
+        hello <span>react</span>
+        <FunctionComponent name="嘿嘿嘿" />
+    </div>
+);
+console.log('---element---');
+console.log(element);
+console.log('-----------');
 
-// ReactDOM.render(title, document.querySelector('#root'))
+ReactDOM.render(element, document.querySelector('#container'));
